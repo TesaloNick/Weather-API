2f8435ddc509fd9f91c2faa046b7da8be80a1c19
Nikolay Antonovich <TesaloNick@gmail.com>
2022-07-12 19:58:30 +0300

    add humidity, wind, pressure. Fix innerHTML

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-const resultCity = document.querySelector('.weather__city')
-const resultTemperature = document.querySelector('.weather__temperature')
-const resultImgWeather = document.querySelector('.weather__img_weather')
-const resultImgFlag = document.querySelector('.weather__img_flag')
-const resultWind = document.querySelector('.weather__wind')
+const weather = document.querySelector('.weather')
 const form = document.querySelector('.form')
 const input = document.querySelector('.form__input')
-// const temperature = document.querySelector('.weather__temperature')
-let lat = 55.75;
-let lon = 37.61;
-let part = 'current';
-let lang = 'ru';
-let time = 1586468027;
+
+// let lat = 55.75;
+// let lon = 37.61;
+// let part = 'current';
+// let lang = 'ru';
+// let time = 1586468027;
 let key = 'ac33dbc75c16a1411070776e8365c1c1';
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=' + key)
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=' + key + '&lang={ru}')
     .then(response => response.json())
     .then(commits => {
-      console.log(commits);
-      resultCity.innerHTML = `Город: ${commits.name}`
-      resultTemperature.innerHTML = `Температура: ${Math.round(commits.main.temp - 273.15)}${String.fromCharCode(0xB0)}C`
-      resultWind.innerHTML = `Скорость ветра: ${commits.wind.speed} м/с`
-      resultImgWeather.setAttribute('src', `http://openweathermap.org/img/wn/${commits.weather[0].icon}.png`)
-      resultImgFlag.setAttribute('src', `http://openweathermap.org/images/flags/${commits.sys.country.toLowerCase()}.png`)
+      console.log(commits, commits.cod);
+      if (+commits.cod === 200) {
+        weather.innerHTML = `
+        <div class="weather__location">
+          <h2 class="weather__city">${commits.name}</h2>
+          <img src="http://openweathermap.org/images/flags/${commits.sys.country.toLowerCase()}.png" alt="" class="weather__img_flag">
+        </div>
+        <p class="weather__temperature">Temperature: ${(commits.main.temp - 273.15).toFixed(2)}${String.fromCharCode(0xB0)}C (feels like ${(commits.main.feels_like - 273.15).toFixed(2)}${String.fromCharCode(0xB0)}C)</p>
+        <p class="weather__wind">Wind speed: ${commits.wind.speed} m/с</p>
+        <p class="weather__visibility">Visibility: ${commits.visibility} m</p>
+        <p class="weather__humidity">Humidity: ${commits.main.humidity}%</p>
+        <p class="weather__pressure">Pressure: ${(commits.main.pressure * 0.7500637).toFixed(0)} mm</p>
+        <div class="weather__description_wrapper">
+          <img src="http://openweathermap.org/img/wn/${commits.weather[0].icon}.png" alt="" class="weather__img_weather">
+          <p class="weather__description">${commits.weather[0].description}</p>
+        </div>
+        `
+      } else {
+        weather.innerHTML = `
+        <h2>Wrong input</h2>
+        `
+      }
     });
+
 })
 
 // fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&dt=${time}&lang=${lang}&units=metric&appid=${key}`)
